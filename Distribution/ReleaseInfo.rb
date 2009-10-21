6b11d10d37160ce67ba33432f888a3e9dbfec2ee
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2009-10-21 10:30:27 +0000

    * Adapted distribution scripts to last release of RubyPackager.
    git-svn-id: https://rdi.svn.sourceforge.net/svnroot/rdi@36 137cb177-7e76-4f49-9f3c-17f339b93885

---
+++ Distribution/ReleaseInfo.rb
@@ -0,0 +1,52 @@
+#--
+# Copyright (c) 2009 Muriel Salvan (murielsalvan@users.sourceforge.net)
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+$ReleaseInfo = RubyPackager::ReleaseInfo.new.
+  author(
+    :Name => 'Muriel Salvan',
+    :EMail => 'murielsalvan@users.sourceforge.net',
+    :WebPageURL => 'http://murielsalvan.users.sourceforge.net'
+  ).
+  project(
+    :Name => 'RDI: Runtime Dependencies Installer',
+    :WebPageURL => 'http://rdi.sourceforge.net/',
+    :Summary => 'Library allowing applications to ensure their dependencies at runtime with UI support.',
+    :Description => 'RDI is a library that gives your application the ability to install its dependencies at runtime. Perfect for plugins oriented architectures. Many kind of deps: RPMs, DEBs, .so, .dll, RubyGems... Easily extensible to new kinds.',
+    :ImageURL => 'http://rdi.sourceforge.net/wiki/images/c/c9/Logo.png',
+    :FaviconURL => 'http://rdi.sourceforge.net/wiki/images/2/26/Favicon.png',
+    :SVNBrowseURL => 'http://rdi.svn.sourceforge.net/viewvc/rdi/',
+    :DevStatus => 'Alpha'
+  ).
+  addCoreFiles( [
+    'lib/**/*'
+  ] ).
+  addTestFiles( [
+    'test/**/*'
+  ] ).
+  addAdditionalFiles( [
+    'README',
+    'LICENSE',
+    'AUTHORS',
+    'Credits',
+    'TODO',
+    'ChangeLog'
+  ] ).
+  gem(
+    :GemName => 'RDI',
+    :GemPlatformClassName => 'Gem::Platform::RUBY',
+    :RequirePath => 'lib',
+    :HasRDoc => true,
+    :TestFile => 'test/run.rb',
+    :GemDependencies => [
+      [ 'rUtilAnts', '>= 0.1' ]
+    ]
+  ).
+  sourceForge(
+    :Login => 'murielsalvan',
+    :ProjectUnixName => 'rdi'
+  ).
+  rubyForge(
+    :ProjectUnixName => 'rdi'
+  )
